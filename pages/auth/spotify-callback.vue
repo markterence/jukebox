ab227e45c964ada3ca55bad3d9e34fe26344811b
markterence <2003215+markterence@users.noreply.github.com>
2022-12-16 00:40:16 +0800

    implementing better spotify oauth flow

---
+++ pages/auth/spotify-callback.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ route.query }}
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+
+onMounted(() => {
+  window.opener.postMessage(
+    {
+      ...route.query,
+
+      target: "spotify-auth-callback",
+    },
+    window.origin
+  );
+});
+</script>
---
+++ pages/auth/spotify.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <spotify-oauth-login-button
+      :scope="scope"
+      :redirectURI="redirectURI"
+    ></spotify-oauth-login-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { SpotifyOAuthScope } from "~~/plugins/spotify/src/constants";
+/**
+ * Handle Spotify Authorization Flow callback
+ */
+import { useStorage } from "@vueuse/core";
+
+const config = useRuntimeConfig();
+const SPOTIFY_CLIENT_ID = config.public.spotifyClientId;
+const redirectURI = "http://localhost:3000/auth/spotify-callback";
+const scope = [
+  SpotifyOAuthScope.streaming,
+  SpotifyOAuthScope.user_read_email,
+  SpotifyOAuthScope.user_read_private,
+  SpotifyOAuthScope.user_library_read,
+  SpotifyOAuthScope.user_modify_playback_state,
+  SpotifyOAuthScope.user_read_playback_state,
+  SpotifyOAuthScope.playlist_read_collaborative,
+  SpotifyOAuthScope.playlist_read_private,
+];
+</script>
